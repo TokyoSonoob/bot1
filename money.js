ac29f39c2249cd8d00b3cadfb723b9e39f98ca85
TokyoSonoob <102928044+TokyoSonoob@users.noreply.github.com>
2025-07-27 01:57:30 +0700

    Update money.js

--- money.js
+++ money.js
@@ -15,5 +15,16 @@ module.exports = function (client) {
 
       await message.channel.send({ embeds: [embed] });
     }
+    if (message.content === "!hi") {
+      await message.delete().catch(console.error);
+      const embed = new EmbedBuilder()
+        .setTitle("⭐ สามารถโอนได้เลยน้าา")
+        .setDescription("ช่องทางการชำระเงิน")
+        .setColor(0x9b59b6)
+        .setImage("https://drive.google.com/uc?export=download&id=1zCjHsfZrAlSCdi7Q23y34BImwCbDbMTU")
+        .setFooter({ text: "Make by Purple Shop" });
+
+      await message.channel.send({ embeds: [embed] });
+    }
   });
 };
