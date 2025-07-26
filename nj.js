999c2b2dbeb2f1be852a4f497e26c845e722b531
TokyoSonoob <102928044+TokyoSonoob@users.noreply.github.com>
2025-07-27 01:46:21 +0700

    Update nj.js

--- nj.js
+++ nj.js
@@ -7,9 +7,9 @@ module.exports = function (client) {
     if (message.content === "!nj") {
       await message.delete().catch(console.error);
       const embed = new EmbedBuilder()
-        .setTitle("test")
-        .setDescription("test")
-        .setColor(0xff6f61)
+        .setTitle("⭐สามารถโอนได้ทั้ง ทรูและวอเลตเลยน้าา")
+        .setDescription("ช่องทางการชำระเงิน")
+        .setColor(0x9b59b6)
         .setImage("https://i.pinimg.com/originals/34/1e/80/341e800b1f29d3e34ea2eba5a6af205c.gif")
         .setFooter({ text: "Make by Purple Shop" });
 
