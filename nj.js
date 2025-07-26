e3acf6e0775765d8e2fa6c728a2a07dfdc118afd
TokyoSonoob <102928044+TokyoSonoob@users.noreply.github.com>
2025-07-27 01:01:11 +0700

    Create nj.js

---
+++ nj.js
@@ -0,0 +1,15 @@
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!nj") {
+    await message.delete().catch(console.error);
+    const embed = new EmbedBuilder()
+      .setTitle("test")
+      .setDescription("test")
+      .setColor(0xff6f61)
+      .setImage("https://i.pinimg.com/originals/34/1e/80/341e800b1f29d3e34ea2eba5a6af205c.gif")
+      .setFooter({ text: "Powered by NJ Bot" });
+
+    await message.channel.send({ embeds: [embed] });
+  }
+});
