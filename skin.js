1777e899ed87e7009b5a9394773818229b4669ea
TokyoSonoob <102928044+TokyoSonoob@users.noreply.github.com>
2025-07-27 18:47:39 +0700

    Update skin.js

--- skin.js
+++ skin.js
@@ -8,13 +8,6 @@ const {
   ActionRowBuilder,
   PermissionsBitField,
 } = require("discord.js");
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-key.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
 
 module.exports = function (client) {
 const express = require("express");
