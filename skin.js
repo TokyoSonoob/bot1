0fba3ffa04806f1c46540d3825f775a4d3d5e4a6
TokyoSonoob <102928044+TokyoSonoob@users.noreply.github.com>
2025-07-28 00:54:03 +0700

    Update skin.js

--- skin.js
+++ skin.js
@@ -8,7 +8,8 @@ const {
   ActionRowBuilder,
   PermissionsBitField,
 } = require("discord.js");
-
+const express = require("express");
+const { db } = require("./firebase");
 module.exports = function (client) {
 
 const PREFIX = "!";
