a0464b500e590d6c2ff5ad5ad268ffcedf89295c
TokyoSonoob <102928044+TokyoSonoob@users.noreply.github.com>
2025-09-01 19:04:42 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,16 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json');
-
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('./firebase-key.json');
+}
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
 }
-
 const db = admin.firestore();
-
 module.exports = {
   db,
   admin
